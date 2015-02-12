a48bed4cd2f213d2135d84d43f78d4a3321ce7a5
mrnu <mrnuu@icloud.com>
2015-02-12 21:34:29 +0000

    Initial commit

---
+++ inc/languages/english/staff_page.lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Staff Page English language pack
+ * mrnu <mrnuu@icloud.com>
+ *
+ */
+
+$l['staff'] = 'Staff';
+$l['no_members'] = 'No members found.';
+$l['no_groups'] = 'No groups found.';
