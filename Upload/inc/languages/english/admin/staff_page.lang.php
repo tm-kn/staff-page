aaef21f1c9774b3a92faee43f70111d67a52ce73
mrnu <mrnuu@icloud.com>
2015-02-13 17:32:29 +0000

    Add DB schema and the ACP page

---
+++ Upload/inc/languages/english/admin/staff_page.lang.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Staff Page English language pack
+ * mrnu <mrnuu@icloud.com>
+ *
+ */
+
+$l['staff'] = 'Staff';
+$l['staff_page'] = 'Staff Page';
+$l['no_members'] = 'No members found.';
+$l['no_groups'] = 'No groups found.';
+$l['staff_page_admin_permission'] = 'Can manage Staff Page';
+$l['add_member'] = 'Add a member';
+$l['add_group'] = 'Add a group';
+
+$l['staff_page_description'] = 'Here you can see all of the groups and members displayed on the Staff Page.';
+$l['add_member_description'] = 'Add a member to the Staff Page.';
+$l['add_group_description'] = 'Add a group to the Staff Page';
+
+$l['name'] = 'Name';
+$l['order'] = 'Order';
+$l['action'] = 'Action';
