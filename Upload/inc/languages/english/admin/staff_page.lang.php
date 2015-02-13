53d9fd2b892357222ba8f2b6fb3db9b098c5e77a
mrnu <mrnuu@icloud.com>
2015-02-13 18:15:41 +0000

    Add adding members and groups in ACP

--- Upload/inc/languages/english/admin/staff_page.lang.php
+++ Upload/inc/languages/english/admin/staff_page.lang.php
@@ -9,7 +9,9 @@ $l['staff'] = 'Staff';
 $l['staff_page'] = 'Staff Page';
 $l['no_members'] = 'No members found.';
 $l['no_groups'] = 'No groups found.';
+
 $l['staff_page_admin_permission'] = 'Can manage Staff Page';
+
 $l['add_member'] = 'Add a member';
 $l['add_group'] = 'Add a group';
 
@@ -18,5 +20,12 @@ $l['add_member_description'] = 'Add a member to the Staff Page.';
 $l['add_group_description'] = 'Add a group to the Staff Page';
 
 $l['name'] = 'Name';
+$l['description'] = 'Description';
 $l['order'] = 'Order';
 $l['action'] = 'Action';
+
+$l['save'] = 'Save';
+
+$l['empty_name'] = 'You must provide the name.';
+$l['wrong_group'] = 'Choose group again.';
+$l['user_not_exist'] = 'User doesn\'t exist.';
